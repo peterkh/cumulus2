123a4193687d3dd31a1cb1cc74d62a3557e351dc
Peter Hall <peter@peterkh.net>
2016-02-07 21:27:06 +1100

    Fixing nose tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,15 @@
 """Test cli module."""
 import argparse
 from cumulus2 import cli
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_equals
 
 
 def test_parse_args_with_cmd():
     """_parse_args(): Namespace object returned with correct action."""
     response = argparse.Namespace(
-        action='create', botologlevel='critical',
-        loglevel='info', stackname=None, yamlfile='cumulus.yaml')
-    assert cli._parse_args(['create']) == response
+        action='create', loglevel='info', stackname=None,
+        yamlfile='cumulus.yaml')
+    assert_equals(cli._parse_args(['create']), response)
 
 
 def test_parse_args_no_cmd():
